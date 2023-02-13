a1616903c85e0382537a5e411a76fdb85b9976f1
david <davmwa90@gmail.com>
2023-02-13 17:51:43 +0300

    completed methods lab

--- lib/song.rb
+++ lib/song.rb
@@ -30,21 +30,6 @@ class Song
         @@artists.uniq  
     end 
 
-#     def self.genre_count 
-#         genre_count = {}
-#         @@genres.map do |genre| 
-#             if genre_count[genre]
-#                 genre_count[genre] +=1
-#             else 
-#                 genre_count[genre] = 1
-#             end  
-#     end 
-#     genre_count
-# end 
-
-    # def self.artist_count 
-    # end 
-
     def self.genre_count
         genre_count = {}
         @@genres.map do |genre|
